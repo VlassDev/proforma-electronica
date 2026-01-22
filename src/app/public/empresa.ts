e75e42132c0e0a2e7fb4098b3153627f6480cc84
Vlass <sarmientovlass@gmail.com>
2026-01-22 14:16:41 -0500

    change 3

--- src/app/public/empresa.ts
+++ src/app/public/empresa.ts
@@ -4,7 +4,7 @@ export const company: Company = {
   name: 'Sarmiento Ingenieros Constructores',
   doc: '20611022566',
   description:
-    'CONSULTORIA, EJECUCIÓN DE OBRAS - VENTA DE MATERIALES DE CONSTRUCCIÓN Y FERRETERÍA EN GENERAL',
+    'CONSULTORIA, EJECUCIÓN DE OBRAS - VENTA DE MATERIALES DE CONSTRUCCIÓN, FERRETERÍA EN GENERAL, Y VENTA DE MATERIALES DE ESCRITORIO AL POR MAYOR Y MENOR',
   phone1: '914 999 548',
   phone2: '967 910 553',
   address: 'Jesús Nazareno - Huamanga - Ayacucho',
